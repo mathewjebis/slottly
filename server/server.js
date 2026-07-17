97a3683a3ad8746fc78ec5205b344a342f20c12c
Mathew Jebis <realmeuser1310@gmail.com>
2026-07-17 11:12:02 +0530

    "feat(middleware): add route protection for future modules

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 app.use("/api/auth", require("./src/routes/authRoutes"));
 
 mongoose
